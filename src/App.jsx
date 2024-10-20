c63cd38fcd10b2e053372fdfc02c70e8d7398ad2
Dilshan <ict21028@std.uwu.ac.lk>
2024-10-20 19:55:10 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.jsx 	new file:   src/assets/images/art.jpg 	new file:   src/assets/images/gavel.jpg 	new file:   src/assets/images/ieee.jpg 	new file:   src/assets/images/image1.jpg 	new file:   src/assets/images/image2.jpg 	new file:   src/assets/images/image3.jpg 	new file:   src/assets/images/leo.jpg 	new file:   src/assets/images/net.jpg 	new file:   src/assets/images/rayon.jpg 	new file:   src/assets/images/rotract.jpg 	new file:   src/assets/images/union.jpg 	new file:   src/assets/pages/About.jsx 	new file:   src/assets/pages/Contact.jsx 	new file:   src/assets/pages/Home.jsx 	new file:   src/assets/pages/Navbar.jsx 	new file:   src/assets/pages/Skills.jsx 	new file:   src/variants.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React from "react";
+import Navbar from "./assets/pages/Navbar";
+import Home from "./assets/pages/Home";
+import About from "./assets/pages/About";
+import Skills from "./assets/pages/Skills";
+import Contact from "./assets/pages/Contact";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-export default function App() {
+function App() {
   return (
-    <div>
-      
-    </div>
-  )
+    <>
+      <Router>
+        <Navbar />
+
+        {/* Routes instead of Switch */}
+        <Routes>
+          {/* Route path matches now require "element" prop */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
+
+         
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
+export default App;
