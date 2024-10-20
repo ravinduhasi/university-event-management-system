0247ae628351d43a04683bd4bf78b35f594b315a
ravinduhasi <ravinduhasithict@gmail.com>
2024-10-20 19:57:15 +0530

    modified:   src/App.jsx 	new file:   src/assets/pages/SigninAndSignup.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Log from './assets/pages/SigninAndSignup';
 
 export default function App() {
   return (
     <div>
-      
+      <Log />
     </div>
   )
 }
---
+++ src/assets/pages/SigninAndSignup.jsx
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+
+const color = {
+    primary: "#060606",
+    background: "#E0E0E0",
+    disabled: "#D9D9D9"
+}
+
+const AuthPage = () => {
+    const [isSignUp, setIsSignUp] = useState(true);
+
+    const toggleForm = () => {
+        setIsSignUp(!isSignUp);
+    };
+
+    return (
+        <div className="flex items-center justify-center w-full h-screen bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+            <div className='w-full max-w-md bg-[#000000] p-8 rounded-[12px] flex flex-col items-center'>
+                <div className='flex flex-col w-full'>
+                    <div className='flex flex-col w-full mb-4 text-center'>
+                        <h3 className='text-2xl font-semibold mb-2 text-[30px] text-[#0284c7]'>{isSignUp ? 'Sign Up' : 'Sign In'}</h3>
+                        <p className='mb-4 text-base text-[#5d5858]'>Please Enter your details.</p>
+                    </div>
+
+                    <div className='flex flex-col w-full'>
+                        {isSignUp && (
+                            <input 
+                                type="text"
+                                placeholder="Enter Your Name"
+                                className='w-full text-[#ffffff] py-2 mb-4 bg-transparent border-b border-[#7b6767] outline-none placeholder-[#ffffff36]'
+                            />
+                        )}
+
+                        <input 
+                            type="email"
+                            placeholder="Enter Your Email"
+                            className='w-full text-[#ffffff] py-2 mb-4 bg-transparent border-b border-[#7b6767] outline-none placeholder-[#ffffff36]'
+                        />
+
+                        {isSignUp && (
+                            <input 
+                                type="text"
+                                placeholder="Enter Your Department"
+                                className='w-full text-[#ffffff] py-2 mb-4 bg-transparent border-b border-[#7b6767] outline-none placeholder-[#ffffff36]'
+                            />
+                        )}
+
+                        {isSignUp && (
+                            <input 
+                                type="text"
+                                placeholder="Enter Your Number"
+                                className='w-full text-[#ffffff] py-2 mb-4 bg-transparent border-b border-[#7b6767] outline-none placeholder-[#ffffff36]'
+                            />
+                        )}
+
+                        <input 
+                            type="password"
+                            placeholder="Enter Your Password"
+                            className='w-full text-[#ffffff] py-2 mb-4 bg-transparent border-b border-[#7b6767] outline-none placeholder-[#ffffff36]'
+                        />
+                    </div>
+
+                    <div className='flex items-center justify-between w-full mb-4'>
+                        <div className='flex items-center'>
+                            <input type="checkbox" className='w-4 h-4 mr-2'/>
+                            <p className='text-sm text-[#7a68689e]'>Remember me</p>
+                        </div>
+                    </div>
+
+                    <button className='w-[350px] ml-5 text-white font-semibold transition ease-in-out delay-150 bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300 rounded-md p-3 text-center mb-4'>
+                        {isSignUp ? 'Sign Up' : 'Login'}
+                    </button>
+
+                    <div className='text-center'>
+                        <p className='text-sm text-[#ffffff]'>
+                            {isSignUp ? 'Already have an account?' : "Don't have an account?"}
+                            <span 
+                                className='font-semibold underline cursor-pointer text-[#5d5858] ml-1'
+                                onClick={toggleForm}
+                            >
+                                {isSignUp ? 'Sign In' : 'Sign Up'}
+                            </span>
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default AuthPage;
