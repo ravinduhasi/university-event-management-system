b80b9003c4124113465a7f56d286c9b461576552
ravinduhasi <ravinduhasithict@gmail.com>
2024-10-20 19:20:29 +0530

    add blog.jsx

---
+++ src/assets/pages/Blog.jsx
@@ -0,0 +1,104 @@
+import React from 'react';
+import home from './samurai.jpg';
+
+const Blog = () => {
+    return (
+        <div className='px-20  py-6  bg-[#ffffff4e] h-full'>
+                <div className='container'>
+                    <div className='mb-5 text-center max-w-[600px]  mx-auto '>
+                        
+                        <p className='text-[45px] font-semibold text-[#073560]'>Club Artical</p>
+                        <h1 className='text-3xl font-bold'>Main Notice</h1>
+                    
+                    </div>
+
+                    {/* body section */}
+                    <div>
+                        <div className='grid grid-cols-1 gap-10 sm:grid-cols-4 md:grid-cols-7 lg:grid-cols-3 place-items-center'>
+                        
+                            {/* card section */}
+                            
+                            <div className='bg-[#ffffff] rounded-lg shadow-2xl h-[350px] sm:h-[350px] md:h-[400px] lg:h-[450px] w-[100px] sm:w-[300px] md:w-[200px] lg:w-[400px]'>
+                                <img
+                                    src={home}
+                                    className='h-[250px] w-full object-cover rounded-md transition duration-700 ease-in-out hover:scale-105 cursor-pointer'
+                                    alt='Samurai'
+                                />
+                                <div>
+                                    <h3 className='font-semibold text-[25px] text-[#06b6d4] p-2'>New Event</h3>
+                                    <p className='p-3 text-sm text-gray-600 gap-7'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid distinctio, beatae voluptas eius laborum recusandae doloremque minima fugit, pariatur voluptatibus hic perferendis voluptatum iste, quas exercitationem aperiam magnam in numquam.</p>
+                                </div>
+                            </div>
+
+
+                            <div className='bg-[#ffffff] rounded-lg shadow-2xl h-[350px] sm:h-[350px] md:h-[400px] lg:h-[450px] w-[100px] sm:w-[300px] md:w-[200px] lg:w-[400px]'>
+                                <img
+                                    src={home}
+                                    className='h-[250px] w-full object-cover rounded-md transition duration-700 ease-in-out hover:scale-105 cursor-pointer'
+                                    alt='Samurai'
+                                />
+                                <div>
+                                    <h3 className='font-semibold text-[25px] text-[#06b6d4] p-2'>New Event</h3>
+                                    <p className='p-3 text-sm text-gray-600 gap-7'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid distinctio, beatae voluptas eius laborum recusandae doloremque minima fugit, pariatur voluptatibus hic perferendis voluptatum iste, quas exercitationem aperiam magnam in numquam.</p>
+                                </div>
+                            </div>
+
+
+
+                            <div className='bg-[#ffffff] rounded-lg shadow-2xl h-[350px] sm:h-[350px] md:h-[400px] lg:h-[450px] w-[100px] sm:w-[300px] md:w-[200px] lg:w-[400px]'>
+                                <img
+                                    src={home}
+                                    className='h-[250px] w-full object-cover rounded-md transition duration-700 ease-in-out hover:scale-105 cursor-pointer'
+                                    alt='Samurai'
+                                />
+                                <div>
+                                    <h3 className='font-semibold text-[25px] text-[#06b6d4] p-2'>New Event</h3>
+                                    <p className='p-3 text-sm text-gray-600 gap-7'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid distinctio, beatae voluptas eius laborum recusandae doloremque minima fugit, pariatur voluptatibus hic perferendis voluptatum iste, quas exercitationem aperiam magnam in numquam.</p>
+                                </div>
+                            </div>
+
+                            
+                            <div className='bg-[#ffffff] rounded-lg shadow-2xl h-[350px] sm:h-[350px] md:h-[400px] lg:h-[450px] w-[100px] sm:w-[300px] md:w-[200px] lg:w-[400px]'>
+                                <img
+                                    src={home}
+                                    className='h-[250px] w-full object-cover rounded-md transition duration-700 ease-in-out hover:scale-105 cursor-pointer'
+                                    alt='Samurai'
+                                />
+                                <div>
+                                    <h3 className='font-semibold text-[25px] text-[#06b6d4] p-2'>New Event</h3>
+                                    <p className='p-3 text-sm text-gray-600 gap-7'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid distinctio, beatae voluptas eius laborum recusandae doloremque minima fugit, pariatur voluptatibus hic perferendis voluptatum iste, quas exercitationem aperiam magnam in numquam.</p>
+                                </div>
+                            </div>
+
+                            <div className='bg-[#ffffff] rounded-lg shadow-2xl h-[350px] sm:h-[350px] md:h-[400px] lg:h-[450px] w-[100px] sm:w-[300px] md:w-[200px] lg:w-[400px]'>
+                                <img
+                                    src={home}
+                                    className='h-[250px] w-full object-cover rounded-md transition duration-700 ease-in-out hover:scale-105 cursor-pointer'
+                                    alt='Samurai'
+                                />
+                                <div>
+                                    <h3 className='font-semibold text-[25px] text-[#06b6d4] p-2'>New Event</h3>
+                                    <p className='p-3 text-sm text-gray-600 gap-7'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid distinctio, beatae voluptas eius laborum recusandae doloremque minima fugit, pariatur voluptatibus hic perferendis voluptatum iste, quas exercitationem aperiam magnam in numquam.</p>
+                                </div>
+                            </div>
+
+                            <div className='bg-[#ffffff] rounded-lg shadow-2xl h-[350px] sm:h-[350px] md:h-[400px] lg:h-[450px] w-[100px] sm:w-[300px] md:w-[200px] lg:w-[400px]'>
+                                <img
+                                    src={home}
+                                    className='h-[250px] w-full object-cover rounded-md transition duration-700 ease-in-out hover:scale-105 cursor-pointer'
+                                    alt='Samurai'
+                                />
+                                <div>
+                                    <h3 className='font-semibold text-[25px] text-[#06b6d4] p-2'>New Event</h3>
+                                    <p className='p-3 text-sm text-gray-600 gap-7'> Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid distinctio, beatae voluptas eius laborum recusandae doloremque minima fugit, pariatur voluptatibus hic perferendis voluptatum iste, quas exercitationem aperiam magnam in numquam.</p>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+        </div>
+    );
+};
+
+export default Blog;
