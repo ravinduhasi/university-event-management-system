e998e01f8af54541202b253cbaf3a14c8feee331
Dilshan <ict21028@std.uwu.ac.lk>
2024-10-20 20:30:20 +0530

    modified:   src/assets/pages/Navbar.jsx

--- src/assets/pages/Navbar.jsx
+++ src/assets/pages/Navbar.jsx
@@ -9,46 +9,46 @@ const Navbar = () => {
     return (
 
         <nav className="flex items-center justify-between h-[70px] shadow-md bg-[#000000e6] px-12">
-            <h3 className="text-2xl font-semibold text-white mb-2">EventAura.</h3>
+            <h3 className="mb-2 text-2xl font-semibold text-white">EventAura.</h3>
             <ul
                 className={`${Mobile ? "flex flex-col absolute bg-white w-full left-0 top-16" : "hidden"
                     } md:flex md:flex-row md:space-x-8 md:w-auto `}
                 onClick={() => setMobile(false)}
             >
                 <li>
-                    <Link to="/" className="text-lg py-2 md:py-0 block text-center text-white mt-1 ">
+                    <Link to="/" className="block py-2 mt-1 text-lg text-center text-white md:py-0 ">
                         Home
                     </Link>
                 </li>
                 <li>
-                    <Link to="/about" className="text-lg py-2 md:py-0 block text-center text-white mt-1">
+                    <Link to="/about" className="block py-2 mt-1 text-lg text-center text-white md:py-0">
                         About
                     </Link>
                 </li>
 
                 <li>
-                    <Link to="/skills" className="text-lg py-2 md:py-0 block text-center text-white mt-1">
+                    <Link to="/skills" className="block py-2 mt-1 text-lg text-center text-white md:py-0">
                         Skills
                     </Link>
                 </li>
                 <li>
-                    <Link to="/contact" className="text-lg py-2 md:py-0 block text-center text-white mt-1">
+                    <Link to="/contact" className="block py-2 mt-1 text-lg text-center text-white md:py-0">
                         Contact
                     </Link>
                 </li>
                 <li>
                     <Link
-                        to="/contact"
+                        to="/signup"
                         className="text-lg py-1 px-6 block text-center text-white border border-white rounded-[60px] transition duration-300 hover:bg-[#ebebeb] hover:text-[#000000]"
                     >
-                        Login
+                        SignUp
                     </Link>
 
                 </li>
             </ul>
 
             <button
-                className="md:hidden text-2xl"
+                className="text-2xl md:hidden"
                 onClick={() => setMobile(!Mobile)}
             >
                 {Mobile ? <ImCross /> : <FaBars />}
