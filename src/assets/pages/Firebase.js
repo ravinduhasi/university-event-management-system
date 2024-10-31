31ddaf1c854b8b8a064cb3c1705458f1f8d6a335
Dilshan <ict21028@std.uwu.ac.lk>
2024-10-31 13:12:00 +0530

    modified:   src/assets/pages/Firebase.js 	deleted:    src/assets/pages/Signup.jsx

--- src/assets/pages/Firebase.js
+++ src/assets/pages/Firebase.js
@@ -1,21 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDMAOxihbj_xn6JckfulpOhW2-p1nG-bC4",
-  authDomain: "event-management-system-ad4d2.firebaseapp.com",
-  projectId: "event-management-system-ad4d2",
-  storageBucket: "event-management-system-ad4d2.appspot.com",
-  messagingSenderId: "461225515446",
-  appId: "1:461225515446:web:3c41aa3e46b8ad02020917"
+  apiKey: "AIzaSyByb3OAi9HHlHQl2DlH9vtig-e9-SLkHZ4",
+  authDomain: "event-management-68adc.firebaseapp.com",
+  projectId: "event-management-68adc",
+  storageBucket: "event-management-68adc.appspot.com",
+  messagingSenderId: "99068419588",
+  appId: "1:99068419588:web:c791f6a3eea4f8027b1f51"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth=getAuth();
-export default app;
