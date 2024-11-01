4d9d773b484947a99804c86f2018b4f1a7a8a4a5
ravinduhasi <ravinduhasithict@gmail.com>
2024-11-01 18:04:25 +0530

    add some features for logging page

--- src/assets/pages/SigninAndSignup.jsx
+++ src/assets/pages/SigninAndSignup.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { auth,db } from './Firebase';
 import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
-import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import { useNavigate } from 'react-router-dom'; 
 import { doc, setDoc } from "firebase/firestore";
 
 
