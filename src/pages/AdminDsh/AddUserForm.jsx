53d3f7c5df4dc1ed31f3898742095f7b0881f7be
Pasindu Dilshan Gamage <pdilshan704@gmail.com>
2024-12-24 21:53:30 +0530

    modified:   src/pages/AdminDsh/AddUserForm.jsx

--- src/pages/AdminDsh/AddUserForm.jsx
+++ src/pages/AdminDsh/AddUserForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { auth, db } from "../Firebase";
+import { auth, db } from "../firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { doc, setDoc } from "firebase/firestore";
 import Navbar from "../../components/Navbar";
