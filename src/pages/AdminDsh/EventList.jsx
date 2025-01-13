cd04426989f5aba40ddc915b77538ba013f1150a
Pasindu Dilshan Gamage <pdilshan704@gmail.com>
2025-01-13 17:00:31 +0530

    modified:   src/pages/AdminDsh/EventList.jsx

--- src/pages/AdminDsh/EventList.jsx
+++ src/pages/AdminDsh/EventList.jsx
@@ -70,27 +70,27 @@ const events = [
 const EventList = () => {
   return (
     <div className="p-4">     
-      <div className="overflow-x-auto max-h-96 overflow-y-scroll">
+      <div className="overflow-x-auto overflow-y-scroll max-h-96">
         <table className="w-full min-w-[640px] table-auto border-collapse">
           <thead className='bg-gray-50'>
             <tr>
-              <th  className="px-6 py-3 text-left text-xs font-light text-gray-500 uppercase tracking-wider">Name</th>
-              <th  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Date</th>
-              <th  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Place</th>
-              <th  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Cost</th>
-              <th  className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Club Name</th>
-              <th  className="px-20 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">Action</th>
+              <th  className="px-6 py-3 text-xs font-light tracking-wider text-left text-gray-500 uppercase">Name</th>
+              <th  className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">Date</th>
+              <th  className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">Place</th>
+              <th  className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">Cost</th>
+              <th  className="px-6 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">Club Name</th>
+              <th  className="px-20 py-3 text-xs font-medium tracking-wider text-left text-gray-500 uppercase">Action</th>
             </tr>
           </thead>
           <tbody>
             {events.map((event, index) => (
               <tr key={index} className="bg-white border-b">
-                <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{event.name}</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{event.date}</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{event.place}</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{event.cost}</td>
-                <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{event.club}</td>
-                <td className="px-20 py-4 whitespace-nowrap text-left text-sm font-medium">
+                <td className="px-6 py-4 text-sm font-medium text-gray-900 whitespace-nowrap">{event.name}</td>
+                <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">{event.date}</td>
+                <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">{event.place}</td>
+                <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">{event.cost}</td>
+                <td className="px-6 py-4 text-sm text-gray-500 whitespace-nowrap">{event.club}</td>
+                <td className="px-20 py-4 text-sm font-medium text-left whitespace-nowrap">
                   <a href="#" className="text-[#000000] hover:text-[#526d82]">View</a>
                 </td>
               </tr>
