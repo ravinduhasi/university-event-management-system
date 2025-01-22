1dd8a08445b1ba459879a8727afa35052ce4a01c
ravinduhasi <ravinduhasithict@gmail.com>
2025-01-22 11:54:14 +0530

    feat: add pricing plans data structure in TicketData component

---
+++ src/pages/price-section/TicketData.jsx
@@ -0,0 +1,50 @@
+export const pricingPlans = [
+  {
+    title: 'Freelancer',
+    price: 24,
+    currency: 'USD',
+    frequency: '/month',
+    description: 'The essentials to provide your best work for clients single user',
+    features: [
+      '5 products',
+      'up to 1,000 subscribers',
+      'Basic analytics',
+      '48-hour support response time',
+    ],
+    cta: 'Start your trial now',
+    mostPopular: false,
+  },
+
+  {
+    title: 'Startup',
+    price: 32,
+    currency: 'USD',
+    frequency: '/month',
+    description: 'A plan that scales with your rapidly growing business',
+    features: [
+      '10 products',
+      'up to 5,000 subscribers',
+      'Advanced analytics',
+      '24-hour support response time',
+    ],
+    cta: 'Start your trial now',
+    mostPopular: true,
+  },
+  
+  {
+    title: 'Enterprise',
+    price: 48,
+    currency: 'USD',
+    frequency: '/month',
+    description: 'Dedicated support and infrastructure for your company',
+    features: [
+      'Unlimited products',
+      'Unlimited subscribers',
+      'Custom analytics',
+      'Priority support',
+    ],
+    cta: 'Start your trial now',
+    mostPopular: false,
+  },
+
+];
