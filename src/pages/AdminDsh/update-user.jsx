fe3864ff988b99a3afd02e246383b669bbb803bd
Pasindu Dilshan Gamage <pdilshan704@gmail.com>
2025-01-23 21:33:54 +0530

    filter users in UpdateUserTable to only include 'user' role

--- src/pages/AdminDsh/update-user.jsx
+++ src/pages/AdminDsh/update-user.jsx
@@ -23,7 +23,7 @@ const UpdateUserTable = () => {
                 const usersSnapshot = await getDocs(collection(db, "users"));
                 const usersData = usersSnapshot.docs
                     .map((doc) => ({ id: doc.id, ...doc.data() }))
-                    .filter((user) => user.role === "user" || user.role === "manager");
+                    .filter((user) => user.role === "user" );
 
                 const managersSnapshot = await getDocs(collection(db, "managers"));
                 const managersData = managersSnapshot.docs
