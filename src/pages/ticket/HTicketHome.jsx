19a1ecada4bf13dbdd1a32cc5827569d91a588ee
Pasindu Dilshan Gamage <pdilshan704@gmail.com>
2025-01-24 18:14:57 +0530

    add event ID display in HTicketHome component

--- src/pages/ticket/HTicketHome.jsx
+++ src/pages/ticket/HTicketHome.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import HTicketPricin from './HTicketPricingPlanes';
 
-
 const Home = () => {
+  const location = useLocation();
+  const eventId = location.state?.eventId; // Access eventId from location.state
+
+  console.log("Received event ID in Home:", eventId); // Log the event ID
+
   return (
-    <div>      
+    <div>
       <HTicketPricin />
+      {eventId && <p>Event ID: {eventId}</p>} {/* Display event ID */}
     </div>
   );
-}
+};
 
 export default Home;
