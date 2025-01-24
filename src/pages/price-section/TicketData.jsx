0cc30e2fd369ad0f8c956ac6772b0c4fbb209a2b
Pasindu Dilshan Gamage <pdilshan704@gmail.com>
2025-01-24 18:14:21 +0530

    update pricing plans to change currency to RS and modify ticket booking details

--- src/pages/price-section/TicketData.jsx
+++ src/pages/price-section/TicketData.jsx
@@ -1,49 +1,39 @@
+
+
 export const pricingPlans = [
   {
     title: 'Freelancer',
     price: 24,
-    currency: 'USD',
-    frequency: '/month',
+    currency: 'RS',
+    venue: 'Online',
+    date: '20th August 2022',
+    time: '10:00 AM',
     description: 'The essentials to provide your best work for clients single user',
-    features: [
-      '5 products',
-      'up to 1,000 subscribers',
-      'Basic analytics',
-      '48-hour support response time',
-    ],
-    cta: 'Start your trial now',
+    cta: 'Book your ticket now',
     mostPopular: false,
   },
 
   {
     title: 'Startup',
     price: 32,
-    currency: 'USD',
-    frequency: '/month',
+    currency: 'RS',
+    venue: 'Online',
+    date: '20th August 2022',
+    time: '10:00 AM',
     description: 'A plan that scales with your rapidly growing business',
-    features: [
-      '10 products',
-      'up to 5,000 subscribers',
-      'Advanced analytics',
-      '24-hour support response time',
-    ],
-    cta: 'Start your trial now',
+    cta: 'Book your ticket now',
     mostPopular: true,
   },
   
   {
     title: 'Enterprise',
     price: 48,
-    currency: 'USD',
-    frequency: '/month',
+    currency: 'RS',
+    venue: 'Online',
+    date: '20th August 2022',
+    time: '10:00 AM',
     description: 'Dedicated support and infrastructure for your company',
-    features: [
-      'Unlimited products',
-      'Unlimited subscribers',
-      'Custom analytics',
-      'Priority support',
-    ],
-    cta: 'Start your trial now',
+    cta: 'Book your ticket now',
     mostPopular: false,
   },
 
